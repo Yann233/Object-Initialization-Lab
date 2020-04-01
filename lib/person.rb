1dad81c529b6062adc534468effa4b16ebf42100
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-01 13:16:21 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,5 @@
+#When #new is called with an argument, 
+#it will pass that argument (or arguments) to the #initialize method
+#and invoke that method. The code in #initialize will then run, 
+#using any arguments from #new.
+
